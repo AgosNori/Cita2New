6b6c218c8e4b2ef3a56156facba060218d9b7751
agos <agosnoriega2018@gmail.com>
2024-07-22 10:47:53 -0300

    borrando el target

--- src/components/NavPrincipal/NavPrincipal.jsx
+++ src/components/NavPrincipal/NavPrincipal.jsx
@@ -38,37 +38,37 @@ function Nav() {
                                 <Link to="#ministerios">Ministerios</Link>
                                 {activeDropdown === "ministerios" && (
                                     <ul className="dropdown-menu">
-                                        <li><Link to="/hombres" target="_blank" rel="noopener noreferrer">Discipulado de hombres</Link></li>
-                                        <li><Link to="/mujeres" target="_blank" rel="noopener noreferrer">Discipulado de mujeres</Link></li>
-                                        <li><Link to="/matrimonios" target="_blank" rel="noopener noreferrer">Matrimonios</Link></li>
-                                        <li><Link to="/citakids" target="_blank" rel="noopener noreferrer">Cita kids</Link></li>
-                                        <li><Link to="/jovenes" target="_blank" rel="noopener noreferrer">Jovenes</Link></li>
-                                        <li><Link to="/reuniones" target="_blank" rel="noopener noreferrer">Reuniones generales</Link></li>
+                                        <li><Link to="/hombres" rel="noopener noreferrer">Discipulado de hombres</Link></li>
+                                        <li><Link to="/mujeres" rel="noopener noreferrer">Discipulado de mujeres</Link></li>
+                                        <li><Link to="/matrimonios" rel="noopener noreferrer">Matrimonios</Link></li>
+                                        <li><Link to="/citakids" rel="noopener noreferrer">Cita kids</Link></li>
+                                        <li><Link to="/jovenes" rel="noopener noreferrer">Jovenes</Link></li>
+                                        <li><Link to="/reuniones" rel="noopener noreferrer">Reuniones generales</Link></li>
                                     </ul>
                                 )}
                             </li>
                             <li>
-                                <Link to="/nosotros" target="_blank" rel="noopener noreferrer">Nosotros</Link>
+                                <Link to="/nosotros" rel="noopener noreferrer">Nosotros</Link>
 
                             </li>
                             <li className="dropdown" onMouseEnter={() => toggleDropdown("vidaiglesia")} onMouseLeave={() => toggleDropdown(null)}>
                                 <Link to="#vidaiglesia">Vida de la iglesia</Link>
                                 {activeDropdown === "vidaiglesia" && (
                                     <ul className="dropdown-menu">
-                                        <li><Link to="/avisos" target="_blank" rel="noopener noreferrer">Avisos importantes</Link></li>
-                                        <li><Link to="/calendario" target="_blank" rel="noopener noreferrer">Calendario</Link></li>
-                                        <li><Link to="/matrimonios" target="_blank" rel="noopener noreferrer">Matrimonios</Link></li>
-                                        <li><Link to="/campamentos" target="_blank" rel="noopener noreferrer">Campamentos</Link></li>
-                                        <li><Link to="/musicos" target="_blank" rel="noopener noreferrer">Escuela de musicos</Link></li>
-                                        <li><Link to="/galeria" target="_blank" rel="noopener noreferrer">Galeria</Link></li>
+                                        <li><Link to="/avisos" rel="noopener noreferrer">Avisos importantes</Link></li>
+                                        <li><Link to="/calendario" rel="noopener noreferrer">Calendario</Link></li>
+                                        <li><Link to="/matrimonios" rel="noopener noreferrer">Matrimonios</Link></li>
+                                        <li><Link to="/campamentos" rel="noopener noreferrer">Campamentos</Link></li>
+                                        <li><Link to="/musicos" rel="noopener noreferrer">Escuela de musicos</Link></li>
+                                        <li><Link to="/galeria" rel="noopener noreferrer">Galeria</Link></li>
                                     </ul>
                                 )}
                             </li>
                             <li>
-                                <Link to="/soynuevo" target="_blank" rel="noopener noreferrer">Soy Nuevo</Link>
+                                <Link to="/soynuevo" rel="noopener noreferrer">Soy Nuevo</Link>
                             </li>
                             <li>
-                                <Link to="/contacto" target="_blank" rel="noopener noreferrer">Contacto</Link>
+                                <Link to="/contacto" rel="noopener noreferrer">Contacto</Link>
                             </li>
                         </ul>
                     </div>

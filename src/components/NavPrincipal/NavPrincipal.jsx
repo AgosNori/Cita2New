d43b3b09e0a992fcc758da58d58dc26582166514
agos <agosnoriega2018@gmail.com>
2024-08-15 14:14:02 -0300

    subiendo codigo del nav responsive

--- src/components/NavPrincipal/NavPrincipal.jsx
+++ src/components/NavPrincipal/NavPrincipal.jsx
@@ -13,17 +13,17 @@ function Nav() {
 
     const toggleMenu = () => {
         if (window.innerWidth <= 768) {
-          setIsMenuOpen(!isMenuOpen);
-          if (!isMenuOpen) {
-            navRef.current.classList.add('responsive_nav');
-          } else {
-            navRef.current.classList.remove('responsive_nav');
-          }
+            setIsMenuOpen(!isMenuOpen);
+            if (!isMenuOpen) {
+                navRef.current.classList.add('responsive_nav_principal');
+            } else {
+                navRef.current.classList.remove('responsive_nav_principal');
+            }
         } else {
-          // Agrega una clase adicional para dispositivos móviles con pantallas más grandes
-          navRef.current.classList.toggle('responsive_nav_large');
+            // Agrega una clase adicional para dispositivos móviles con pantallas más grandes
+            navRef.current.classList.toggle('responsive_nav_large');
         }
-      };
+    };
     const showNavbar = () => {
         navRef.current.classList.toggle("responsive_nav");
     };
@@ -37,54 +37,59 @@ function Nav() {
     return (
         <>
             <header className="headerNav">
-                
-                <div data-aos="fade-right">
-                    <img className="logoNav" src='./Logo RGB BLANCO.png' alt="Logo" />
+                <div className="background-video">
+                    <video src="./kidsabados.mp4" controls loop muted width="100%"></video>
                 </div>
-                <nav ref={navRef} className={`navNav ${isMenuOpen ? "responsive_nav" : ""}`}>
-                    <div data-aos="fade-left">
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li className="dropdown" onMouseEnter={() => toggleDropdown("ministerios")} onMouseLeave={() => toggleDropdown(null)}>
-                                <Link to="#ministerios">Ministerios</Link>
-                                {activeDropdown === "ministerios" && (
-                                    <ul className="dropdown-menu">
-                                        <li><Link to="/hombres" rel="noopener noreferrer">Discipulado de hombres</Link></li>
-                                        <li><Link to="/mujeres" rel="noopener noreferrer">Discipulado de mujeres</Link></li>
-                                        <li><Link to="/matrimonios" rel="noopener noreferrer">Matrimonios</Link></li>
-                                        <li><Link to="/citakids" rel="noopener noreferrer">Cita kids</Link></li>
-                                        <li><Link to="/jovenes" rel="noopener noreferrer">Jovenes</Link></li>
-                                        <li><Link to="/reuniones" rel="noopener noreferrer">Reuniones generales</Link></li>
-                                    </ul>
-                                )}
-                            </li>
-                            <li>
-                                <Link to="/nosotros" rel="noopener noreferrer">Nosotros</Link>
-
-                            </li>
-                            <li className="dropdown" onMouseEnter={() => toggleDropdown("vidaiglesia")} onMouseLeave={() => toggleDropdown(null)}>
-                                <Link to="#vidaiglesia">Vida de la iglesia</Link>
-                                {activeDropdown === "vidaiglesia" && (
-                                    <ul className="dropdown-menu">
-                                        <li><Link to="/avisos" rel="noopener noreferrer">Avisos importantes</Link></li>
-                                        <li><Link to="/calendario" rel="noopener noreferrer">Calendario</Link></li>
-                                        <li><Link to="/campamento" rel="noopener noreferrer">Campamentos</Link></li>
-                                        <li><Link to="/musicos" rel="noopener noreferrer">Escuela de musicos</Link></li>
-                                        <li><Link to="/galeria" rel="noopener noreferrer">Galeria</Link></li>
-                                    </ul>
-                                )}
-                            </li>
-                            <li>
-                                <Link to="/soynuevo" rel="noopener noreferrer">Soy Nuevo</Link>
-                            </li>
-                            <li>
-                                <Link to="/contacto" rel="noopener noreferrer">Contacto</Link>
-                            </li>
-                        </ul>
+                <div className="nav">
+                    <div data-aos="fade-right">
+                        <img className="logoNav" src='./Logo RGB BLANCO.png' alt="Logo" />
                     </div>
-                </nav>
+                    <nav ref={navRef} className={`navNav ${isMenuOpen ? "responsive_nav_principal" : ""}`}>
+                        <div data-aos="fade-left">
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li className="dropdown" onMouseEnter={() => toggleDropdown("ministerios")} onMouseLeave={() => toggleDropdown(null)}>
+                                    <Link to="#ministerios">Ministerios</Link>
+                                    {activeDropdown === "ministerios" && (
+                                        <ul className="dropdown-menu">
+                                            <li><Link to="/hombres" rel="noopener noreferrer">Discipulado de hombres</Link></li>
+                                            <li><Link to="/mujeres" rel="noopener noreferrer">Discipulado de mujeres</Link></li>
+                                            <li><Link to="/matrimonios" rel="noopener noreferrer">Matrimonios</Link></li>
+                                            <li><Link to="/citakids" rel="noopener noreferrer">Cita kids</Link></li>
+                                            <li><Link to="/jovenes" rel="noopener noreferrer">Jovenes</Link></li>
+                                            <li><Link to="/reuniones" rel="noopener noreferrer">Reuniones generales</Link></li>
+                                        </ul>
+                                    )}
+                                </li>
+                                <li>
+                                    <Link to="/nosotros" rel="noopener noreferrer">Nosotros</Link>
+
+                                </li>
+                                <li className="dropdown" onMouseEnter={() => toggleDropdown("vidaiglesia")} onMouseLeave={() => toggleDropdown(null)}>
+                                    <Link to="#vidaiglesia">Vida de la iglesia</Link>
+                                    {activeDropdown === "vidaiglesia" && (
+                                        <ul className="dropdown-menu">
+                                            <li><Link to="/avisos" rel="noopener noreferrer">Avisos importantes</Link></li>
+                                            <li><Link to="/calendario" rel="noopener noreferrer">Calendario</Link></li>
+                                            <li><Link to="/campamento" rel="noopener noreferrer">Campamentos</Link></li>
+                                            <li><Link to="/musicos" rel="noopener noreferrer">Escuela de musicos</Link></li>
+                                            <li><Link to="/galeria" rel="noopener noreferrer">Galeria</Link></li>
+                                        </ul>
+                                    )}
+                                </li>
+                                <li>
+                                    <Link to="/soynuevo" rel="noopener noreferrer">Soy Nuevo</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contacto" rel="noopener noreferrer">Contacto</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+
                 <button className="nav-btn-principal" onClick={toggleMenu}>
                     {isMenuOpen ? <FaTimes /> : <FaBars />}
                 </button>

a9a2c714fa85f2976d0aa4dea75ff9a80debca6a
agos <agosnoriega2018@gmail.com>
2024-08-31 14:41:44 -0300

    finalizando pagina de mujeres

--- src/components/Mujeres/Mujeres.jsx
+++ src/components/Mujeres/Mujeres.jsx
@@ -27,8 +27,8 @@ function Mujeres() {
         <div className='cardsInfo'>
           <div className='fondoCard'>
             <div className='cardMujeres'>
-              <h2 className='discipuladosMuj'>Discipulados de mujeres</h2>
-              <p className='DescripMuj'>Un espacio donde las mujeres pueden crecer espiritualmente, fortalecer sus relaciones con Dios y entre ellas, y ser equipadas para enfrentar los desafíos de la vida cotidiana con fe y confianza. Te esperamos!</p>
+     
+              <p className='DescripMuj'>Más bien, que la belleza de ustedes sea la incorruptible, la que procede de lo íntimo del corazón y consiste en un espiritu humilde y apacible. Esta sí que tiene mucho valor delante de Dios.<br/><br/> 1 Pedro 3:4 (NVI)</p>
             </div>
           </div>
         </div>
@@ -40,8 +40,8 @@ function Mujeres() {
         <div className='cardsInfo'>
           <div className='fondoCard'>
             <div className='cardMujeres'>
-              <h2 className='discipuladosMuj'>Discipulados de mujeres</h2>
-              <p className='DescripMuj'>Un espacio donde las mujeres pueden crecer espiritualmente, fortalecer sus relaciones con Dios y entre ellas, y ser equipadas para enfrentar los desafíos de la vida cotidiana con fe y confianza. Te esperamos!</p>
+              <h2 className='discipuladosMuj'>Con que te encontrarás</h2>
+              <p className='DescripMuj'>- Enseñanza biblica <br/> - Apoyo y Comunidad <br/> - Oración y alabanza</p>
             </div>
           </div>
         </div>
